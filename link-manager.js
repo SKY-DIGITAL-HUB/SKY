d8bef62e07d5419b2d7a8b6b465147cfc0155fc2
sunilkumar_25 <suniltamil2025@gmail.com>
2024-12-31 07:33:35 +0530

    Update link-manager.js

--- link-manager.js
+++ link-manager.js
@@ -3,10 +3,10 @@ async function fetchMetadata() {
   return response.ok ? response.json() : null;
 }
 
-async function getLinksForFile(repository, file) {
+async function getLinksForFile(MBA, fIN.html) {
   const metadata = await fetchMetadata();
   if (metadata && metadata.repositories[repository] && metadata.repositories[repository][file]) {
-    return metadata.repositories[repository][file];
+    return metadata.repositories[MBA][fin.html];
   }
   return {};
 }
